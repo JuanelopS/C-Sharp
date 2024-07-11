9703e72177fc2c2f38f30e4254c409b97e534553
Juan Gavira <juan.gavira@e-zigurat.com>
2024-07-11 15:03:25 +0200

    more exercises

--- methods/Program.cs
+++ methods/Program.cs
@@ -1,3 +1,8 @@
-﻿Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+﻿Random dice = new();
+int roll1 = dice.Next();
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(101);
+Console.Clear();
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
